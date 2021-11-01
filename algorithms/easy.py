be1393ab8035024cc74ea77a3a3f351fbb9b963e
daddyfreud <frederic.sautot@papernest.com>
2021-11-01 16:04:12 +0100

    easyy

---
+++ algorithms/easy.py
@@ -0,0 +1,84 @@
+from typing import List, Optional
+
+
+class Solution:
+    def twoSumNoob(self, nums: List[int], target: int) -> Optional[List[int]]:
+        pivot = 0
+        while pivot < len(nums) - 1:
+            k = pivot + 1
+            while k < len(nums):
+                if nums[pivot] + nums[k] == target:
+                    return [pivot, k]
+                k += 1
+            pivot += 1
+        return
+
+    def twoSum1(self, nums: List[int], target: int) -> Optional[List[int]]:
+        hashmap = {}  # index: value
+        for i, n in enumerate(nums):
+            if target - n in hashmap.values():
+                return [i, list(hashmap.values()).index(target - n)]
+            hashmap[i] = n
+        return
+
+    def twoSum(self, nums: List[int], target: int) -> Optional[List[int]]:
+        prevmap = {}  # value: index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevmap:
+                return [i, prevmap[diff]]
+            prevmap[n] = i
+        return
+
+    def isPalindrome(self, x: int) -> bool:
+        return list(str(x)) == list(str(x))[::-1]
+
+    def romanToIntNoob(self, s: str) -> int:
+        romanmap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1_000}
+        subtractions = {
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900,
+        }
+        spec = []
+        sum = 0
+        for c in s:
+            if spec:
+                spec.extend([c])
+                specialsub = "".join(spec)
+                if specialsub in subtractions:
+                    sum += subtractions[specialsub] - romanmap[spec[0]]
+                    spec = []
+                    continue
+                spec = []
+            if c in ("I", "X", "C"):
+                spec.append(c)
+            sum += romanmap[c]
+        return sum
+
+    def romanToInt1(self, s: str) -> int:
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        s = s.replace("IV", "I" * 4).replace("IX", "I" * 9)
+        s = s.replace("XL", "X" * 4).replace("XC", "X" * 9)
+        s = s.replace("CD", "C" * 4).replace("CM", "C" * 9)
+
+        res = 0
+        for char in s:
+            res += roman_dict[char]
+
+        return res
+
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        for i in range(0, len(s) - 1):
+            if roman_dict[s[i]] >= roman_dict[s[i + 1]]:
+                sum += roman_dict[s[i]]
+            else:
+                sum -= roman_dict[s[i]]
+        sum += roman_dict[s[len(s) - 1]]
+        return sum
---
+++ main.py
@@ -0,0 +1,6 @@
+from algorithms.easy import Solution
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.romanToInt("MDCCCLXXXIV"))
