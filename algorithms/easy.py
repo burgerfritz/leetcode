d6806f052ffb9fb9940d0e5b56f5c6b58f96e7ea
daddyfreud <frederic.sautot@papernest.com>
2021-11-05 23:07:05 +0100

    add longest common prefix

--- algorithms/easy.py
+++ algorithms/easy.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List, Optional
 
 
@@ -82,3 +83,29 @@ class Solution:
                 sum -= roman_dict[s[i]]
         sum += roman_dict[s[len(s) - 1]]
         return sum
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs.count(strs[0]) == len(strs):
+            return strs[0]
+
+        i = 0
+        output = []
+        while True:
+            common = defaultdict(int)
+            for s in strs:
+                if not s:
+                    return ""
+                try:
+                    s[i]
+                except IndexError:
+                    return "".join(output)
+
+                common[s[i]] += 1
+
+            for k, v in common.items():
+                if v == len(strs):
+                    output.append(k)
+                else:
+                    return "".join(output)
+
+            i += 1
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from algorithms.easy import Solution
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.romanToInt("MDCCCLXXXIV"))
+    # print(s.romanToInt("MDCCCLXXXIV"))
+    print(s.longestCommonPrefix(["ab", "a"]))
